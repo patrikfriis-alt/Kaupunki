fffaf50bec7ff0bd2c7c17829902c689b7d49cca
patrikfriis-alt <patrik.friis@gmail.com>
2026-02-28 15:19:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -620,10 +620,12 @@ const server = http.createServer(async (req, res) => {
       return;
     }
 
-    if (req.url.match(/^\/news\/(arctial|kokkola)$/)) {
-      const aihe = req.url.split('/')[2];
+    if (req.url.match(/^\/news\/(arctial|kokkola)(\?.*)?$/)) {
+      const urlObj = new URL(req.url, 'http://localhost');
+      const aihe   = urlObj.pathname.split('/')[2];
+      const limit  = Math.min(parseInt(urlObj.searchParams.get('limit') || '10', 10) || 10, 100);
       try {
-        const data = await supabaseGet(`uutiset?aihe=eq.${encodeURIComponent(aihe)}&order=id.desc&limit=10`);
+        const data = await supabaseGet(`uutiset?aihe=eq.${encodeURIComponent(aihe)}&order=id.desc&limit=${limit}`);
         res.statusCode = 200;
         res.end(JSON.stringify(data));
       } catch (err) {
